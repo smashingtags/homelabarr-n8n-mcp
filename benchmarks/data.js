bf197c85dd35a247cf07e969574cf51ed17442bb
github-action-benchmark <github@users.noreply.github.com>
2025-07-31 16:08:59 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 23327f5dc72270cf77cd87779779fdd237c7a15c

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753977631132,
+  "lastUpdate": 1753978139564,
   "repoUrl": "https://github.com/czlonkowski/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -2116,6 +2116,58 @@ window.BENCHMARK_DATA = {
             "unit": "ms",
             "range": 0.32420000000000004,
             "extra": "15094 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "56956555+czlonkowski@users.noreply.github.com",
+            "name": "Romuald Członkowski",
+            "username": "czlonkowski"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "23327f5dc72270cf77cd87779779fdd237c7a15c",
+          "message": "Merge pull request #106 from czlonkowski/fix/docker-config-file-support\n\nfix: add Docker configuration file support (fixes #105)",
+          "timestamp": "2025-07-31T18:07:48+02:00",
+          "tree_id": "ee973173090c4b224f364e2e1f313c9c23d7bf89",
+          "url": "https://github.com/czlonkowski/n8n-mcp/commit/23327f5dc72270cf77cd87779779fdd237c7a15c"
+        },
+        "date": 1753978139261,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0188,
+            "unit": "ms",
+            "range": 0.3092,
+            "extra": "53192 ops/sec"
+          },
+          {
+            "name": "sample - array sorting - large",
+            "value": 3.1504,
+            "unit": "ms",
+            "range": 0.5278999999999998,
+            "extra": "317 ops/sec"
+          },
+          {
+            "name": "sample - string concatenation",
+            "value": 0.005,
+            "unit": "ms",
+            "range": 0.27340000000000003,
+            "extra": "200516 ops/sec"
+          },
+          {
+            "name": "sample - object creation",
+            "value": 0.0668,
+            "unit": "ms",
+            "range": 0.3655,
+            "extra": "14963 ops/sec"
           }
         ]
       }
